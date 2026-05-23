dbcf5c8659010f68594fada80e7fee5c81c48a8c
MasterA5 <agustin090806090806@gmail.com>
2026-05-23 12:54:16 -0300

    Add scheduling functionality for desktop and android notifications in example app

--- examples/example.py
+++ examples/example.py
@@ -1,7 +1,12 @@
-import os
-from pathlib import Path
+from datetime import timedelta
 
-from flet_local_notifications import FletLocalNotification, DesktopNotificationConfig, AndroidNotificationConfig
+from flet_local_notifications import (
+    FletLocalNotification, 
+    DesktopNotificationConfig, 
+    AndroidNotificationConfig, 
+    DesktopScheduleNotificationConfig, 
+    AndroidScheduleNotificationConfig
+)
 from flet import *
 
 def main(page: Page):
@@ -13,10 +18,23 @@ def main(page: Page):
 
     async def send(e: ControlEvent):
         await notification_manager.send(
-            desktop_config=DesktopNotificationConfig("hello", "hello", app_name="Nose"),
+            desktop_config=DesktopNotificationConfig("hello", "hello", app_name="Flet App"),
             android_config=AndroidNotificationConfig("hello", "hello"),
             on_sent=lambda: (page.controls.append(Text("Notification Sented !!")), page.update())
         )
+    
+    async def send_schedule(e: ControlEvent):
+        await notification_manager.schedule(
+            desktop_schedule_config=DesktopScheduleNotificationConfig(
+                desktop_config=DesktopNotificationConfig("hello", "hello", app_name="Flet App"),
+                notify_time=timedelta(seconds=5) # <- 5 secconds for wait
+            ),
+            android_schedule_config=AndroidScheduleNotificationConfig(
+                android_config=AndroidNotificationConfig("hello", "hello"),
+                notify_time=timedelta(seconds=5) # <- 5 secconds for wait
+            )
+        )
+    
 
-    page.add(Button("Send", on_click=send))
+    page.add(Button("Send", on_click=send), Button("Send Schedule", on_click=send_schedule))
 app(target=main)
